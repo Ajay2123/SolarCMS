bfec8b45e730b35250826432432da82ad1c76ef2
ajay kumar Rajamundry <ajayraj2187@gmail.com>
2018-10-15 16:52:01 +0200

    Angular Material Design Added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import{FormsModule} from'@angular/forms';
+import{BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
 import { ProductListComponent } from './products/product-list.component';
 import { ConvertToSpacesPipe } from './shared/convertToSpaces.pipe';
+import { MaterialModule } from './shared/material.module';
+import { MatIconModule } from '@angular/material';
+
+
 
 
 @NgModule({
   imports: [
+    BrowserAnimationsModule,
     BrowserModule,
-    FormsModule
+    FormsModule,
+    MaterialModule,
+    MatIconModule
+
   ],
   declarations: [
     AppComponent,
